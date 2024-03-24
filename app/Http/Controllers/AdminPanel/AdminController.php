0c258faf55fa128882f15f1b1bfb630323e1221d
zakwannato <86301006+zakwannato@users.noreply.github.com>
2024-03-24 22:41:03 +0800

    Admin dashboard

--- app/Http/Controllers/AdminPanel/AdminController.php
+++ app/Http/Controllers/AdminPanel/AdminController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\AdminPanel;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function index()
     {
         $user = auth()->user();
-        return view('admin.dashboard')->with('user',$user);
+        $DashUsers =  User::select('location', DB::raw('count(*) as total'))
+        ->where('role','user')
+        ->groupBy('location')
+        ->get();
+
+        return view('admin.dashboard')->with('user',$user)->with('DashUsers',$DashUsers);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,7 +2,26 @@
 
 @section('content')
 
-<h1>Admin Dashboard</h1>
-
+<div class="container bg-white py-4">
+    <h1>User Distribution by Location</h1>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Location</th>
+                    <th>Total Users</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($DashUsers as $DashUser)
+                <tr>
+                    <td>{{ $DashUser->location }}</td>
+                    <td>{{ $DashUser->total }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 
 @endsection
